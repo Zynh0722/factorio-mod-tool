5ae01319f8792daf701bc11c469770d7a58d04ce
Zynh0722 <Zynh0722@gmail.com>
2023-05-30 22:16:06 -0700

    getting default mods directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use clap::Parser;
+
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short = 'f', long = "file")]
+    /// File path of factorio mods folder
+    path: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let mods_folder = args
+        .path
+        .unwrap_or_else(|| get_default_mods_folder().unwrap());
+
+    println!("{mods_folder:?}");
+}
+
+#[derive(Debug, Clone)]
+struct NoDefaultPathError {}
+
+fn get_default_mods_folder() -> Result<PathBuf, NoDefaultPathError> {
+    #[cfg(not(any(target_os = "windows", target_os = "macos", target_os = "linux")))]
+    return NoDefaultPathError;
+
+    #[cfg(target_os = "linux")]
+    return Ok(dirs::home_dir().unwrap().join(".factorio/mods"));
+
+    #[cfg(any(target_os = "macos", target_os = "windows"))]
+    let config_path = dirs::config_dir().unwrap();
+
+    #[cfg(target_os = "macos")]
+    return Ok(config_path.join("factorio/mods"));
+
+    #[cfg(target_os = "windows")]
+    return Ok(config_path.join("Factorio/mods"));
 }
