2b85efe672bac225e25b7eef1635c4cf33713ccb
Zynh0722 <Zynh0722@gmail.com>
2023-06-03 21:32:33 -0700

    Adding subcommand extract

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,41 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 use std::path::PathBuf;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    #[command(subcommand)]
+    command: Option<Command>,
+
     #[arg(short = 'f', long = "file")]
     /// File path of factorio mods folder
     path: Option<PathBuf>,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Extract {
+        /// Fetch both enabled and disabled mods
+        ///
+        /// By default extract only grabs enabled mods
+        #[arg(short, long, default_value_t = true)]
+        all: bool,
+
+        /// Fetch the json mod list used by factorio
+        ///
+        /// This effectively just cats the mod-list.json file
+        /// from the mods folder
+        ///
+        /// This ignores --all as the mod list contains the
+        /// enabled metadata within it
+        #[arg(short, long, default_value_t = false)]
+        list: bool,
+
+        /// Set the target file for the extracted mods or mod list
+        #[arg(short, long)]
+        output: Option<PathBuf>,
+    },
 }
 
 fn main() {
