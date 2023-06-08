d2af58dee0ef30a001f5735be55dafc7968c64a4
Zynh0722 <Zynh0722@gmail.com>
2023-06-08 03:35:21 -0700

    Some parsing is happening

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,45 @@ use default_folder::get_default_mods_folder;
 
 use std::{fs, path::PathBuf};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct ModFile {
     pub file_name: String,
     pub path: PathBuf,
+    pub data: ModFileData,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+enum ModFileData {
+    ModList,
+    ModSettings,
+    Mod(String),
+    #[allow(dead_code)]
+    Uknown,
 }
 
 impl ModFile {
-    fn new(p: PathBuf) -> Self {
+    fn new(p: &PathBuf) -> Self {
+        let file_name = p.file_name().unwrap().to_str().unwrap().to_string();
+        let split_name_version = file_name.clone();
+        let mut split_name_version = split_name_version.split("_");
+        let name = split_name_version.next().unwrap();
+
+        let data = match name {
+            "mod-list.json" => ModFileData::ModList,
+            "mod-settings.dat" => ModFileData::ModSettings,
+            name => {
+                if let Some(mod_name) = name.split("_").next() {
+                    ModFileData::Mod(mod_name.to_owned())
+                } else {
+                    ModFileData::Uknown
+                }
+            }
+        };
+
         Self {
             path: p.clone(),
-            file_name: p.file_name().unwrap().to_str().unwrap().to_string(),
+            file_name,
+            data,
         }
     }
 }
@@ -39,9 +67,38 @@ fn main() {
 
     let mods_folder = fs::read_dir(mods_folder).unwrap().flatten();
 
-    let mods = mods_folder.map(|p| ModFile::new(p.path()));
+    let mods_folder_contents: Vec<ModFile> = mods_folder.map(|p| ModFile::new(&p.path())).collect();
 
-    for module in mods {
-        println!("{:?}\n\t{:?}\n", module.file_name, module.path);
-    }
+    let mods: Vec<ModFile> = mods_folder_contents
+        .iter()
+        .filter(|file| matches!(file.data, ModFileData::Mod(_)))
+        .cloned()
+        .collect();
+
+    let _mod_list = mods_folder_contents
+        .iter()
+        .find(|f| f.data == ModFileData::ModList)
+        .unwrap()
+        .clone();
+
+    let _mod_settings = mods_folder_contents
+        .iter()
+        .find(|f| f.data == ModFileData::ModSettings)
+        .unwrap()
+        .clone();
+
+    let unknown_files: Vec<ModFile> = mods_folder_contents
+        .iter()
+        .filter(|f| f.data == ModFileData::Uknown)
+        .cloned()
+        .collect();
+
+    println!("# Mods: {}", mods.iter().count());
+    println!(
+        "# Files - Mods = {} # should be two",
+        mods_folder_contents.iter().count() - mods.iter().count()
+    );
+    println!("# ModList: Detected");
+    println!("# ModSettings: Detected");
+    println!("# Uknowns: {}", unknown_files.iter().count());
 }
