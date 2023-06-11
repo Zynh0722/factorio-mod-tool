dcc6b0be3cc7433f55b3037b816ee97fb3c88708
Zynh0722 <Zynh0722@gmail.com>
2023-06-11 04:46:22 -0700

    extract file reading to function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
-use std::fs::{self, DirEntry};
+use std::{
+    fs::{self, DirEntry},
+    path::PathBuf,
+};
 
 mod args;
 mod init;
@@ -23,15 +26,18 @@ fn main() {
     assert!(mods_folder_path.exists());
 
     let mods_folder = fs::read_dir(mods_folder_path).unwrap();
-    let mods_folder: Vec<DirEntry> = mods_folder.map(|dir| dir.unwrap()).collect();
+    let _mods_folder: Vec<DirEntry> = mods_folder.map(|dir| dir.unwrap()).collect();
 
-    let contents = fs::read_to_string(mods_folder_path.join("mod-list.json"))
-        .expect("Should have been able to read the file");
-
-    // println!("With text:\n{contents}");
-    let mod_list: ModList = serde_json::from_str(&contents).expect(r"This didn't work ¯\_(ツ)_/¯");
+    let mod_list_path = mods_folder_path.clone().join("mod-list.json");
+    let mod_list: ModList = try_parse_mod_list(&mod_list_path).unwrap();
 
     println!("{:#?}", mod_list)
+}
+
+fn try_parse_mod_list(path: &PathBuf) -> std::io::Result<ModList> {
+    let contents = fs::read_to_string(path)?;
+
+    Ok(serde_json::from_str(&contents)?)
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
