25b583b165f9cf2933ea90a255622132cdfefa81
Zynh0722 <Zynh0722@gmail.com>
2023-06-11 02:56:46 -0700

    more forward progress!

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,17 @@ fn main() {
 
     let mods_folder = args.path.or_else(get_default_mods_folder);
 
+    if mods_folder.is_none() {
+        println!("Unable to find mods folder. (see --help for more info)");
+        return;
+    }
+
+    let mods_folder = mods_folder.unwrap();
+
+    let mods_folder_exists = mods_folder.exists();
+
     println!("{mods_folder:?}");
+    println!("exists? {mods_folder_exists:?}");
 }
 
 fn get_default_mods_folder() -> Option<PathBuf> {
